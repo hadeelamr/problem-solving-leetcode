73253f497f2683681885c07942a5fd8cff6e83e1
hadeelamr <82241787+hadeelamr@users.noreply.github.com>
2025-08-05 12:53:24 +0300

    Time: 6 ms (57.23%) | Memory: 60 MB (39.55%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+
+    if (numRows === 1 || s.length <= numRows) return s;
+
+    const rows = Array(Math.min(numRows, s.length)).fill('');
+    let currentRow = 0;
+    let goingDown = false;
+
+     for (let c of s) {
+        rows[currentRow] += c;
+        if (currentRow === 0 || currentRow === numRows - 1) goingDown = !goingDown;
+        currentRow += goingDown ? 1 : -1;
+    }
+        return rows.join('');
+
+};
