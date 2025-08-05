970fba4a5d5ee4f8635cedfd2b9f5d2fdf093a5d
hadeelamr <82241787+hadeelamr@users.noreply.github.com>
2025-08-05 12:40:40 +0300

    Time: 0 ms (100.00%) | Memory: 53.4 MB (85.19%) - LeetSync

---
+++ 274-h-index/h-index.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+ // new
+var hIndex = function(citations) {
+    const n = citations.length;
+    const count = new Array(n + 1).fill(0);
+
+    for (let c of citations) {
+        if (c >= n) {
+            count[n]++;
+        } else {
+            count[c]++;
+        }
+    }
+
+    let total = 0;
+    for (let i = n; i >= 0; i--) {
+        total += count[i];
+        if (total >= i) return i;
+    }
+
+    return 0;
+};
+
+        
