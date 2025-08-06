487ee008511264f91752b84959611a458eed1b91
hadeelamr <82241787+hadeelamr@users.noreply.github.com>
2025-08-06 22:19:19 +0300

    Time: 8 ms (44.76%) | Memory: 59.5 MB (29.97%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+     let stack = [];
+
+    for (let token of tokens) {
+        if (["+", "-", "*", "/"].includes(token)) {
+            let b = stack.pop();
+            let a = stack.pop();
+            let result;
+
+            if (token === "+") result = a + b;
+            else if (token === "-") result = a - b;
+            else if (token === "*") result = a * b;
+            else if (token === "/") result = Math.trunc(a / b); 
+
+            stack.push(result);
+        } else {
+            stack.push(parseInt(token));
+        }
+    }
+
+    return stack.pop();
+};
