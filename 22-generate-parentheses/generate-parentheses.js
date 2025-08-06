cc756b18702a4a0e7812e714806679f5c781b726
hadeelamr <82241787+hadeelamr@users.noreply.github.com>
2025-08-06 22:20:42 +0300

    Time: 2 ms (43.10%) | Memory: 55.2 MB (32.04%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+    let result = [];
+
+    function backtrack(current, open, close) {
+        if (current.length === n * 2) {
+            result.push(current);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(current + "(", open + 1, close);
+        }
+        if (close < open) {
+            backtrack(current + ")", open, close + 1);
+        }
+    }
+
+    backtrack("", 0, 0);
+    return result;
+};
