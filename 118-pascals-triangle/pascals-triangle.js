010b1cad57e05c7f93d0743dfdfdaa3672f9938b
hadeelamr <82241787+hadeelamr@users.noreply.github.com>
2025-08-19 23:07:54 +0300

    Time: 0 ms (100.00%) | Memory: 55.7 MB (18.28%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+     let res = [];
+
+    for (let i = 0; i < numRows; i++) {
+        let row = new Array(i + 1).fill(1); // كل صف يبدأ بـ 1 وينتهي بـ 1
+        for (let j = 1; j < i; j++) {
+            row[j] = res[i - 1][j - 1] + res[i - 1][j]; 
+        }
+        res.push(row);
+    }
+
+    return res;
+};
